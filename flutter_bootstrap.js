9313941eedbe86723550c384e65872afd374944a
Satvik <satvikfake@gmail.com>
2026-04-16 19:29:39 +0530

    Built web
    Branch: main
    Commit: dd8f575ed02aa3854b11c321eda7c774a712406a (dirty)
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2333392672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1360058996" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
